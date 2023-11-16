7b170d8553338ff755ab18b5663f82c3ba3cbcac
Abdullah Alqahtani <amnalqahtani@elm.sa>
2023-11-16 18:44:28 +0300

    add datasource to the Application class

--- src/main/java/com/abdull/database/DatabaseApplication.java
+++ src/main/java/com/abdull/database/DatabaseApplication.java
@@ -1,13 +1,28 @@
 package com.abdull.database;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class DatabaseApplication {
+@Log
+public class DatabaseApplication implements CommandLineRunner {
 
+	private final DataSource dataSource;
+
+	public DatabaseApplication(final DataSource dataSource) {this.dataSource = dataSource;}
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
 }
