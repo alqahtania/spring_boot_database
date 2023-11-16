39c79de51a47908c2547f03792a91f2f0fe93c8c
Abdullah Alqahtani <amnalqahtani@elm.sa>
2023-11-16 18:42:19 +0300

    add author and book daos and their implementation

---
+++ src/main/java/com/abdull/database/dao/AuthorDao.java
@@ -0,0 +1,4 @@
+package com.abdull.database.dao;
+
+public interface AuthorDao {
+}
---
+++ src/main/java/com/abdull/database/dao/impl/BookDaoImpl.java
@@ -0,0 +1,13 @@
+package com.abdull.database.dao.impl;
+
+import com.abdull.database.dao.BookDao;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class BookDaoImpl implements BookDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
