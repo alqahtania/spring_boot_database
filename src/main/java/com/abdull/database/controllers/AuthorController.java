2dd60bfc0eb2e3677131d80e33aca9414691a260
Abdullah Alqahtani <amnalqahtani@elm.sa>
2023-11-18 09:47:57 +0300

    finish integration test for AuthorController using mock mvc

--- src/main/java/com/abdull/database/controllers/AuthorController.java
+++ src/main/java/com/abdull/database/controllers/AuthorController.java
@@ -5,6 +5,8 @@ import com.abdull.database.entity.AuthorEntity;
 import com.abdull.database.mappers.Mapper;
 import com.abdull.database.service.AuthorService;
 import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +25,9 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto authorDto) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto authorDto) {
         AuthorEntity authorEntity = authorMapper.mapFrom(authorDto);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 }
